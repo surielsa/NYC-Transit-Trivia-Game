938ba0ab17db9fdb60f0f7756bcb692fa3477320
surielsa <surielsa@gmail.com>
2019-04-22 14:30:56 -0400

    final revisions

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # TriviaGame
+This Trivia Game is about the NYC Transit system. We all use it and it is complicated and ambivalent. It is convinient yet not as reliable as we would all hope it to be. 
+Below is the Key to the Quiz. How well did you do?
+
+Answers
+1. A
+2. C
+3. False
+4. Q
+5. True
+6. True
+7. 42nd Street Times Square
+8. 2
+9. 6
+10. 3
---
+++ assets/javascript/app.js
@@ -0,0 +1,193 @@
+$(document).ready(function(){
+  
+    // event listeners
+    $("#remaining-time").hide();
+    $("#start").on('click', trivia.startGame);
+    $(document).on('click' , '.option', trivia.guessChecker);
+    
+  })
+  
+  var trivia = {
+    // trivia properties
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    currentSet: 0,
+    timer: 20,
+    timerOn: false,
+    timerId : '',
+    // questions options and answers data
+    questions: {
+      q1: "I am trying to get to Macy's Herald Square. Which of these subways trains cannot take straight there?",
+      q2: "I'm trying to get to Harlem, and I need a train to take me there. Which one of these subway lines should I take?",
+      q3: "The 2 train goes to all five New York City boroughs.",
+      q4: "Which NYC subway train is NOT a part of the Eighth Avenue line",
+      q5: "The 7 train goes to both 42nd Street Times Square and 42nd STreet Grand Central. True or false?",
+      q6: "Essex Street and DElancey Street subway are the same place",
+      q7: "Which of these NY subway train stations is nearest the Port Authority Bus Terminal?",
+      q8: "I am trying to go to Times Square to get to the M&M store. What NY subway train can I take there?",
+      q9: "I'm trying to find the St. Patrick's Cathedral on Fifth Avenue. Which number train can I take there?",
+      q10: "I'm really trying to find a book, and I'm in Brooklyn right now. Which subway train should I take to get to Grand Army Plaza Lirbrary?"
+    },
+    options: {
+      q1: ['A','M','3','8'],
+      q2: ['C','F','G','7'],
+      q3: ['True','False'],
+      q4: ['C','A','E','Q'],
+      q5: ['True','False'],
+      q6: ['True','False'],
+      q7: ['42nd St. Times Square', 'Atlantic Avenue Station', '42nd St. Grand CEntral Station','34th St. Herald Square'],
+      q8: ['G','2','V','Z'],
+      q9: ['2','6','4','9'],
+      q10: ['A','3','F','L'] 
+    },
+    answers: {
+      q1: 'A',
+      q2: 'C',
+      q3: 'False',
+      q4: 'Q',
+      q5: 'True',
+      q6: 'True',
+      q7: '42nd Street Times Square',
+      q8: '2',
+      q9: '6',
+      q10: '3'
+    },
+    // trivia methods
+    // method to initialize game
+    startGame: function(){
+      // restarting game results
+      trivia.currentSet = 0;
+      trivia.correct = 0;
+      trivia.incorrect = 0;
+      trivia.unanswered = 0;
+      clearInterval(trivia.timerId);
+      
+      // show game section
+      $('#game').show();
+      
+      //  empty last results
+      $('#results').html('');
+      
+      // show timer
+      $('#timer').text(trivia.timer);
+      
+      // remove start button
+      $('#start').hide();
+  
+      $('#remaining-time').show();
+      
+      // ask first question
+      trivia.nextQuestion();
+      
+    },
+    // method to loop through and display questions and options 
+    nextQuestion : function(){
+      
+      // set timer to 20 seconds each question
+      trivia.timer = 10;
+       $('#timer').removeClass('last-seconds');
+      $('#timer').text(trivia.timer);
+      
+      // to prevent timer speed up
+      if(!trivia.timerOn){
+        trivia.timerId = setInterval(trivia.timerRunning, 1000);
+      }
+      
+      // gets all the questions then indexes the current questions
+      var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+      $('#question').text(questionContent);
+      
+      // an array of all the user options for the current question
+      var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+      
+      // creates all the trivia guess options in the html
+      $.each(questionOptions, function(index, key){
+        $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
+      })
+      
+    },
+    // method to decrement counter and count unanswered if timer runs out
+    timerRunning : function(){
+      // if timer still has time left and there are still questions left to ask
+      if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
+        $('#timer').text(trivia.timer);
+        trivia.timer--;
+          if(trivia.timer === 4){
+            $('#timer').addClass('last-seconds');
+          }
+      }
+      // the time has run out and increment unanswered, run result
+      else if(trivia.timer === -1){
+        trivia.unanswered++;
+        trivia.result = false;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#results').html('<h3>Missed the train! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+      }
+      // if all the questions have been shown end the game, show results
+      else if(trivia.currentSet === Object.keys(trivia.questions).length){
+        
+        // adds results of game (correct, incorrect, unanswered) to the page
+        $('#results')
+          .html('<h3>Thank you for playing!</h3>'+
+          '<p>Correct: '+ trivia.correct +'</p>'+
+          '<p>Incorrect: '+ trivia.incorrect +'</p>'+
+          '<p>Unaswered: '+ trivia.unanswered +'</p>'+
+          '<p>Please play again!</p>');
+        
+        // hide game sction
+        $('#game').hide();
+        
+        // show start button to begin a new game
+        $('#start').show();
+      }
+      
+    },
+    // method to evaluate the option clicked
+    guessChecker : function() {
+      
+      // timer ID for gameResult setTimeout
+      var resultId;
+      
+      // the answer to the current question being asked
+      var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+      
+      // if the text of the option picked matches the answer of the current question, increment correct
+      if($(this).text() === currentAnswer){
+        // turn button green for correct
+        $(this).addClass('btn-success').removeClass('btn-info');
+        
+        trivia.correct++;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#results').html('<h3>Correct Answer!</h3>');
+      }
+      // else the user picked the wrong option, increment incorrect
+      else{
+        // turn button clicked red for incorrect
+        $(this).addClass('btn-danger').removeClass('btn-info');
+        
+        trivia.incorrect++;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
+      }
+      
+    },
+    // method to remove previous question results and options
+    guessResult : function(){
+      
+      // increment to next question set
+      trivia.currentSet++;
+      
+      // remove the options and results
+      $('.option').remove();
+      $('#results h3').remove();
+      
+      // begin next question
+      trivia.nextQuestion();
+       
+    }
+  
+  }
